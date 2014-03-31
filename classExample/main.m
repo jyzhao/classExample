acec1f5ef291647445c483cbd2681b0ef20ef48b
Jiyuan Zhao <zhao89@purdue.edu>
2014-03-31 00:52:12 -0400

    Initial Commit

---
+++ classExample/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  classExample
+//
+//  Created by 赵骥远 on 14-3-31.
+//  Copyright (c) 2014年 Jiyuan Zhao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
